9b19527c88c49ceb602867950be7a684d33b609c
Nawdist <78550645+Nawdist@users.noreply.github.com>
2021-02-21 23:25:30 +0100

    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -2,31 +2,28 @@ var buttons = document.querySelectorAll(".button");
 var result = document.getElementById("result");
 buttons.forEach(function(item){
     item.addEventListener("click", function(){
-        result.innerHTML += item.innerHTML;
+        result.value += item.innerHTML;
     })
 })
-var clear = document.getElementById("clear").addEventListener('click', function(){
-    result.innerHTML = "";
-})
+var finalResult = document.getElementById("equal").onclick = function(){
+    if(result.value == ""){
+        result.className = "wrong";
+        result.value = "";
+        setTimeout(function(){
+            result.className ="input";
+        }, 3000)
+        
+    } else {
+        result.value = eval(result.value);
+    }
+}
 var back = document.getElementById('back');
 back.addEventListener('click', function(){
-    result.innerHTML = result.innerHTML.slice(0, -1);
+    result.value = result.value.slice(0, -1);
 })
-var shift = document.getElementById('shift');
-var shifted = false;
-shift.addEventListener("click", function(){
-    if(shifted == false){
-        buttons.forEach(function(element){
-            element.innerHTML = element.innerHTML.toUpperCase();
-        })
-        shifted = true; 
-      } else if (shifted == true){
-        buttons.forEach(function(element){
-            element.innerHTML = element.innerHTML.toLowerCase();
-        })
-        shifted = false;
-    }
+var clear = document.getElementById("clear").addEventListener('click', function(){
+    result.value = "";
 })
-var ok = document.getElementById('go').addEventListener('click', function(){
+var go = document.getElementById('go').addEventListener('click', function(){
     window.location.href = ('../index.html')
 })
